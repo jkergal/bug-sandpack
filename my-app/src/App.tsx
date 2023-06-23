ac7331bc37ba8ab2b4b52204160cf4f708c5c2f3
jkergal <hello@johannkergal.fr>
2023-06-23 10:25:44 +0200

    init project + adds simple sandpack implementation with bug

---
+++ my-app/src/App.tsx
@@ -0,0 +1,49 @@
+import { SandpackCodeEditor, SandpackConsole, SandpackFileExplorer, SandpackPreview } from '@codesandbox/sandpack-react';
+import { SandpackLayout, SandpackProvider } from '@codesandbox/sandpack-react';
+import './App.css';
+
+function App() {
+  return (
+    <SandpackProvider
+      style={{ height: '100vh' }}
+      files={{ '/PROUT.tsx': '' }}
+      // template={undefined}
+    >
+        <SandpackLayout
+          style={{
+            width: '100%',
+            height: '100%',
+            borderRadius: '0',
+            border: 'none'
+          }}
+        >
+            <SandpackFileExplorer 
+              style={{
+                height: '100%'
+              }}
+            />
+            <SandpackCodeEditor
+              closableTabs={true}
+              style={{
+                width: '100%',
+                height: '100%'
+              }}
+            />
+            <SandpackPreview
+              showNavigator
+              showOpenInCodeSandbox={false}
+              style={{
+                height: '100%'
+              }}
+            />
+            <SandpackConsole
+              style={{
+                height: '100%'
+              }}
+            />
+        </SandpackLayout>
+  </SandpackProvider>
+  );
+}
+
+export default App;
